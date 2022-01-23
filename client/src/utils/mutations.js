b611ef282e001da426270569fc2d543b7635cbe7
lilys0 <lsouryaphanh@gmail.com>
2022-01-23 23:01:10 +1100

    queries and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,58 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook(
+    $bookId: ID!
+    $authors: [String!]
+    $description: String!
+    $image: String!
+    $link: String!
+  ) {
+    addBook(
+      bookId: $bookId
+      authors: $authors
+      description: $description
+      image: $image
+      link: $link
+    ) {
+      bookId
+      authors
+      description
+      image
+      link
+    }
+  }
+`;
+
+//check this
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(book: $bookId) {
+      
+    }
+  }
+`;
