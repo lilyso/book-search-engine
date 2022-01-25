e00dfc2ae95fc5b46fa59fdf4e83d24a09520d0d
lilys0 <lsouryaphanh@gmail.com>
2022-01-26 04:11:51 +1100

    remove build

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb+srv://lilyso:rgk6mkp%40KQM8ukq%40zmf@cluster0.oqtvu.mongodb.net/test?authSource=admin&replicaSet=atlas-7d2dv8-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;
