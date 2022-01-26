1d9643f41923048a0f2ab3a7ebc028196705ab67
lilys0 <lsouryaphanh@gmail.com>
2022-01-26 14:40:31 +1100

    heroku deploy

--- server/server.js
+++ server/server.js
@@ -22,8 +22,12 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
 
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
+
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 db.once("open", () => {
