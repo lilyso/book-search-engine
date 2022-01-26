ef2b213033f435adcf739b64ac582726d6c5b8e3
lilys0 <lsouryaphanh@gmail.com>
2022-01-26 14:48:51 +1100

    heroku deploy

--- server/server.js
+++ server/server.js
@@ -12,19 +12,14 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware,
 });
-console.log("db", process.env.MONGODB_URI);
 server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "..client/build")));
 }
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
